d17ad3f5bef7b2d4fe4390a5c2fe567a808fb101
Barry Ke <35748027+BarryKeee@users.noreply.github.com>
2023-11-11 16:28:29 -0500

    add readme

--- empirical_analysis/Main_empirical.m
+++ empirical_analysis/Main_empirical.m
@@ -92,7 +92,7 @@ empirical_application(FOMC_sec, func, save_fig_name, ...
 
 %% Baseline prior for FOMC2
 % percent difference
-FOMC_sec = 1;
+FOMC_sec = 2;
 func = @HHI_percent_diff;
 prior_posterior_spec = "prior_alpha_1.25_beta_0.025";
 prior_post_draw_name = NMF_draw_parent_folder + '\' + prior_posterior_spec ...
@@ -128,8 +128,8 @@ prior_post_draw_name = NMF_draw_parent_folder + '\' + prior_posterior_spec ...
     + '\B_Theta_post_draws_sec1.mat'; 
 NMF_draw_folder_name = NMF_draw_parent_folder + '\' + prior_posterior_spec ...
     + '\FOMC1\NMF_Theta';
-x_lims = [20, 60];
-y_lims = [0, 0.45];
+x_lims = [55, 120];
+y_lims = [0, 0.4];
 x_label = '%';
 density_type = "Posterior";
 save_fig_name = prior_posterior_spec + '_percent_diff';
@@ -141,8 +141,8 @@ empirical_application(FOMC_sec, func, save_fig_name, ...
 func = @HHI_regression;
 save_fig_name = prior_posterior_spec + '_regression';
 x_label = 'Regression Coef on Transparency';
-x_lims = [0.004, 0.022];
-y_lims = [0, 800];
+x_lims = [0.025, 0.06];
+y_lims = [0, 600];
 disp(save_fig_name)
 empirical_application(FOMC_sec, func, save_fig_name, ...
     prior_post_draw_name, NMF_draw_folder_name, x_lims, y_lims, x_label, density_type)
@@ -156,7 +156,7 @@ prior_post_draw_name = NMF_draw_parent_folder + '\' + prior_posterior_spec ...
     + '\B_Theta_post_draws_sec1.mat'; 
 NMF_draw_folder_name = NMF_draw_parent_folder + '\' + prior_posterior_spec ...
     + '\FOMC1\NMF_Theta';
-x_lims = [-6, 6];
+x_lims = [-8, 8];
 y_lims = [0, 0.45];
 x_label = '%';
 density_type = "Posterior";
